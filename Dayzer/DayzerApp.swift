fe2944a87d17aac989859b981abb1685f1a15db0
SaujanBindukar <sauzanbindukar@gmail.com>
2022-11-08 20:23:22 +0545

    Initial Commit

---
+++ Dayzer/DayzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayzerApp.swift
+//  Dayzer
+//
+//  Created by Saujan Bindukar on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct DayzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
